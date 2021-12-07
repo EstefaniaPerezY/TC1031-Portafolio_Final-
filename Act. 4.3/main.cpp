293fc045ef340f989c8a4008b1dfcfd207ad2d7a
EstefaniaPerezY <83040053+EstefaniaPerezY@users.noreply.github.com>
2021-12-07 00:49:30 -0600

    Add files via upload

---
+++ Act. 4.3/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ Created by: 
+ Estefanía Pérez Yeo A01639270
+ Alan Paul García Rosales A01639631
+
+ Compilacion para debug:  
+  g++ -std=c++17 -g -o main *.cpp 
+
+ Ejecucion con valgrind:
+  valgrind --leak-check=full ./main 
+
+ Compilacion para ejecucion:  
+  g++ -std=c++17 -O3 -o main *.cpp 
+
+ Ejecucion:
+  ./main
+*/
+
+#include <iostream>
+#include "Graph.h"
+int main(){
+  Graph graphBinnacle("bitacoraGrafos.txt");
+}
